5b07d2d18b8f5dc1b1263077b610c52836bfd019
Magnus Ottenklinger <evnu@posteo.de>
2019-05-14 22:54:34 +0200

    Generate helptags

--- update.sh
+++ update.sh
@@ -5,6 +5,15 @@
 
 git submodule init
 
+mkdir -p bundle
+
 git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
     cut -f 2 -d ' ' |
     xargs git submodule update
+
+# requires pathogen
+vim +Helptags +qall
+
+if which nvim &> /dev/null; then
+    nvim +Helptags +qall
+fi
