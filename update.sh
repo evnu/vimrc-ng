604957886a664e323e841bf61a9b0df7e4cf95ae
Magnus Ottenklinger <evnu@posteo.de>
2019-10-09 21:31:29 +0200

    Fix generating helptags and silence nvim

--- update.sh
+++ update.sh
@@ -9,8 +9,8 @@ git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
     cut -f 2 -d ' ' |
     xargs git submodule update
 
-vim +helptags ALL +qall
+vim +helptags pack/ +qall
 
 if which nvim &> /dev/null; then
-    nvim +helptags ALL +qall
+    nvim +helptags pack/ +qall > /dev/null
 fi
